5df3f84442369b3900a935a5b347c0fcd7696e67
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2022-12-26 18:49:11 -0300

    Construct project structure

---
+++ src/Messenger.Contracts/Message.cs
@@ -0,0 +1,20 @@
+﻿namespace Messenger.Contracts
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+
+        public Message()
+        {
+            Id = Guid.NewGuid();
+            Content = string.Empty;
+        }
+
+        public Message(string conttent)
+        {
+            Id = Guid.NewGuid();
+            Content = conttent;
+        }
+    }
+}
